8cfd9f024377a7bf4c4a93116e7350d15fdf760b
aosborne17 <AOsborne@spartaglobal.com>
2020-07-08 19:21:21 +0100

    first commit

--- ibi_text_file.py
+++ ibi_text_file.py
@@ -1,14 +1,14 @@
 
-def writetextfilesusingwith(self):
-    with open(order_text, "w+") as file, open(writer_text, "w+") as file2:
-        try:
-            file_input = (input("Enter your file input:"))
-            if len(file_input) == 0:
-                raise Exception("sorry we dont take empty names")
-        except Exception:
-            print("you did not enter anything")
-            self.writetextfilesusingwith()
-        else:
-            file.write(file_input)
-        file.seek(0)
-        file2.write((file.read()))
+# def writetextfilesusingwith(self):
+#     with open(order_text, "w+") as file, open(writer_text, "w+") as file2:
+#         try:
+#             file_input = (input("Enter your file input:"))
+#             if len(file_input) == 0:
+#                 raise Exception("sorry we dont take empty names")
+#         except Exception:
+#             print("you did not enter anything")
+#             self.writetextfilesusingwith()
+#         else:
+#             file.write(file_input)
+#         file.seek(0)
+#         file2.write((file.read()))
